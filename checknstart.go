b8062a4bb23063599e26dc7986efd0779ea69a27
Marc Chauffour <karmoc@gmail.com>
2017-09-24 16:29:20 +0200

    Gros update
    Ajout des options wait and launch
    Possibilité de travailler en local (sans net use)
    Vérification de la valeur clef en Registre

--- checknstart.go
+++ checknstart.go
@@ -12,6 +12,18 @@
 // - voir si la notion runonce, run, runif est à prendre en compte
 // - Dialogue de confirmation Yes/No ou OK simple
 // 	- Détails possible sur la session en cours (variable d'environnement Horizon)
+//
+// Plus pragmatique et immédiat :
+// On va gérer les aller-retour entre les fichiers de bases de données SQL anywhere
+// locaux et distants.
+// Plus tard on fera évoluer le programme en un utilitaire plus complet/complexe.
+//
+// Command Line sample
+// checknstart.exe -setdefault -user pi -pwd xxxxxx -share wd1to -remotefile autorun.inf
+//                 -localfile c:\tools\autorun.inf -cmd sublime
+//                 -regkey "HKCU\Volatile Environment\1\test" -delay 10
+//
+
 package main
 
 import (
@@ -26,6 +38,8 @@ import (
 	"strings"
 	"time"
 
+	"golang.org/x/sys/windows/registry"
+
 	"github.com/dustin/go-humanize"
 	"github.com/efarrer/iothrottler"
 )
@@ -34,29 +48,54 @@ import (
 // Like an Args container
 type (
 	context struct {
-		endpoint    *string
-		share       *string
-		remotename  *string
-		localname   *string
-		cmd         *string
-		user        *string
-		pwd         *string
-		limitstring *string
-		reverse     *bool
-		limit       uint64
-		verbose     *bool
-		remoteinfo  os.FileInfo
-		localinfo   os.FileInfo
-		refreshneed bool
-		starttime   time.Time
-		endtime     time.Time
+		endpoint       *string
+		share          *string
+		remotename     *string
+		localname      *string
+		cmd            *string
+		user           *string
+		pwd            *string
+		backupcmd      *string
+		backupbase     *string
+		backupuser     *string
+		backuppwd      *string
+		waitingfor     *string
+		howlong        *int64
+		tosuccessful   *bool
+		setdefault     *bool
+		uploadmode     *bool
+		limitgetstring *string
+		limitget       uint64
+		limitputstring *string
+		limitput       uint64
+		verbose        *bool
+		remoteinfo     os.FileInfo
+		localinfo      os.FileInfo
+		refreshneed    bool
+		starttime      time.Time
+		endtime        time.Time
 	}
 )
 
 // contexte : Hold runtime value (from commande line args)
 var contexte context
 
+// Valeur par défaut si le paramètre est non renseigné et setdefault "activé"
 const dacqname = "DACQ"
+const endpointdefval = "ViewClient_Machine_Name"
+const sharedefval = "kheops"
+const remotenamedefval = "darwinsav.db"
+const localnamedefval = "c:\\b3s\\dacq\\base\\darwinsav.db"
+const cmddefval = "c:\\b3s\\dacq\\application\\dacq.exe"
+const userdefval = "DACQ"
+const pwddefval = "DACQ"
+const backupcmddefval = "c:\\b3s\\sql anywhere 5.0\\win32\\dbbackup.exe"
+const backupbasedefval = "darwin"
+const backupuserdefval = "adm"
+const backuppwddefval = "sql"
+const waitingfordefval = "HKU\\ralala\\dateread"
+const limitgetdefval = "10mb"
+const limitputdefval = "32k"
 
 // Copy one file at once
 // src : Source file to copy
@@ -149,15 +188,19 @@ func getFiles(src string) (filesOut []os.FileInfo, errOut error) {
 }
 
 func getRemotePath(ctx *context) string {
+	// si on veut spécifier un path local (pas net use)
+	if *ctx.endpoint == "" && *ctx.share == "" {
+		return fmt.Sprintf("%s", *ctx.remotename)
+	}
 	return fmt.Sprintf("\\\\%s\\%s\\%s", *ctx.endpoint, *ctx.share, *ctx.remotename)
 }
 
 // Copy one file to another file
 func copyOneFile(ctx *context) (written int64, err error) {
-	if *ctx.reverse {
-		return copyFileContents(ctx.localinfo.Size(), *ctx.localname, getRemotePath(ctx), ctx.limit)
+	if *ctx.uploadmode {
+		return copyFileContents(ctx.localinfo.Size(), *ctx.localname, getRemotePath(ctx), ctx.limitput)
 	}
-	return copyFileContents(ctx.remoteinfo.Size(), getRemotePath(ctx), *ctx.localname, ctx.limit)
+	return copyFileContents(ctx.remoteinfo.Size(), getRemotePath(ctx), *ctx.localname, ctx.limitget)
 }
 
 // No more Wildcard and selection in this Array
@@ -200,13 +243,15 @@ func getFileSpec(src string, lib string, verbose bool) (os.FileInfo, error) {
 }
 
 // Check if remote file exist
-func remoteFileHere(ctx *context) (err error) {
-	out, err := mapDrive(fmt.Sprintf("\\\\%s\\%s", *ctx.endpoint, *ctx.share), *ctx.user, *ctx.pwd, *ctx.verbose)
-	if err != nil {
-		if *ctx.verbose {
-			log.Println(string(out))
+func remoteFileHere(ctx *context) error {
+	if *ctx.share != "" || *ctx.endpoint != "" {
+		out, err := mapDrive(fmt.Sprintf("\\\\%s\\%s", *ctx.endpoint, *ctx.share), *ctx.user, *ctx.pwd, *ctx.verbose)
+		if err != nil {
+			if *ctx.verbose {
+				log.Println(string(out))
+			}
+			return fmt.Errorf("Can't map remote share on \\\\%s\\%s", *ctx.endpoint, *ctx.share)
 		}
-		return fmt.Errorf("Can't map remote share on \\\\%s\\%s", *ctx.endpoint, *ctx.share)
 	}
 	finfo, err := getFileSpec(getRemotePath(ctx), "remote", *ctx.verbose)
 	ctx.remoteinfo = finfo
@@ -222,7 +267,7 @@ func compareFileAge(ctx *context) (bool, error) {
 	}
 	ctx.localinfo = finfo
 
-	if *ctx.reverse {
+	if *ctx.uploadmode {
 		ltime = ctx.remoteinfo.ModTime()
 		rtime = ctx.localinfo.ModTime()
 	} else {
@@ -240,16 +285,27 @@ func compareFileAge(ctx *context) (bool, error) {
 
 // Prepare Command Line Args parsing
 func setFlagList(ctx *context) {
-	ctx.endpoint = flag.String("endpoint", "", "Physical remote device (versus current VDI)")
-	ctx.share = flag.String("share", "kheops", "Share name on endpoint")
-	ctx.remotename = flag.String("remotefile", "darwinsav.db", "Source filename to check & get (no wildcard)")
-	ctx.localname = flag.String("localfile", "c:\\b3s\\dacq\\base\\darwinsav.db", "Target Filename for copy (no wildcard)")
-	ctx.cmd = flag.String("cmd", "c:\\b3s\\dacq\\application\\dacq.exe", "Target cmd when ready")
-	ctx.user = flag.String("user", "", "User account to use share on endpoint")
-	ctx.pwd = flag.String("pwd", "", "Password account to use share on endpoint")
-	ctx.limitstring = flag.String("limit", "10MB", "Bytes per second limit (default 10MB/s unlimited)")
+	ctx.setdefault = flag.Bool("setdefault", false, "Must be use default value if empty")
+	ctx.endpoint = flag.String("endpoint", "", fmt.Sprintf("Physical remote device (versus current VDI) [env %s]", endpointdefval))
+	ctx.share = flag.String("share", "", fmt.Sprintf("Share name on endpoint [%s]", sharedefval))
+	ctx.remotename = flag.String("remotefile", "", fmt.Sprintf("Source filename to check & get (no wildcard) [%s]", remotenamedefval))
+	ctx.localname = flag.String("localfile", "", fmt.Sprintf("Target Filename for copy (no wildcard) [%s]", localnamedefval))
+	ctx.cmd = flag.String("cmd", "", fmt.Sprintf("Target cmd when ready [%s]", cmddefval))
+	ctx.user = flag.String("user", "", fmt.Sprintf("User account to use share on endpoint [%s]", userdefval))
+	ctx.pwd = flag.String("pwd", "", "Password account to use share on endpoint [***]")
+	ctx.limitgetstring = flag.String("getrate", "", fmt.Sprintf("Download bytes per second limit [%s]", limitgetdefval))
+	ctx.limitputstring = flag.String("putrate", "", fmt.Sprintf("Upload bytes per second limit [%s]", limitputdefval))
 	ctx.verbose = flag.Bool("verbose", false, "Verbose mode")
-	ctx.reverse = flag.Bool("reverse", false, "Reverse mode. Source become Target.")
+	ctx.uploadmode = flag.Bool("upload", false, "Upload mode: Source become Target/no execution at end/use sqlcmd.")
+	// gestion du backup SQL Anywhere
+	ctx.backupcmd = flag.String("sqlcmd", "", fmt.Sprintf("Backup tools full path [%s]", backupcmddefval))
+	ctx.backupbase = flag.String("sqlbase", "", fmt.Sprintf("SQL anywhere database name [%s]", backupbasedefval))
+	ctx.backupuser = flag.String("sqluser", "", fmt.Sprintf("SQL anywhere user account [%s]", backupuserdefval))
+	ctx.backuppwd = flag.String("sqlpwd", "", "SQL anwyhere password account [***]")
+	ctx.waitingfor = flag.String("regkey", "", fmt.Sprintf("Registry item to check [%s]", waitingfordefval))
+	ctx.howlong = flag.Int64("delay", 5*60, "Checking delay")
+	ctx.tosuccessful = flag.Bool("timeoutok", true, "Timeout is it an option?")
+
 	flag.Parse()
 }
 
@@ -265,26 +321,138 @@ func processArgs(ctx *context) (err error) {
 		return fmt.Errorf("remote name can't include wildcard: %s", *ctx.remotename)
 	}
 
-	ctxlimit, err := humanize.ParseBytes(*ctx.limitstring)
-	if err != nil {
-		return fmt.Errorf("Limit value - Error:%s", err) // handle error
+	if *ctx.setdefault {
+		if *ctx.pwd == "" {
+			*ctx.pwd = pwddefval
+		}
+		if *ctx.user == "" {
+			*ctx.user = userdefval
+		}
+		if *ctx.endpoint == "" {
+			*ctx.endpoint = os.Getenv(endpointdefval)
+		}
+		if *ctx.share == "" {
+			*ctx.share = sharedefval
+		}
+		if *ctx.localname == "" {
+			*ctx.localname = localnamedefval
+		}
+		if *ctx.cmd == "" {
+			*ctx.cmd = cmddefval
+		}
+		if *ctx.backupcmd == "" {
+			*ctx.backupcmd = backupcmddefval
+		}
+		if *ctx.backupbase == "" {
+			*ctx.backupbase = backupbasedefval
+		}
+		if *ctx.backupuser == "" {
+			*ctx.backupuser = backupuserdefval
+		}
+		if *ctx.backuppwd == "" {
+			*ctx.backuppwd = backuppwddefval
+		}
+		if *ctx.waitingfor == "" {
+			*ctx.waitingfor = waitingfordefval
+		}
 	}
-	ctx.limit = ctxlimit
+	// pour les limites, il n'y a pas de setdefault à positionner
+	if *ctx.limitgetstring == "" {
+		*ctx.limitgetstring = limitgetdefval
+	}
+
+	if *ctx.limitputstring == "" {
+		*ctx.limitputstring = limitputdefval
+	}
+
+	ctxlimitget, err := humanize.ParseBytes(*ctx.limitgetstring)
+	if err != nil {
+		return fmt.Errorf("GetLimit value - Error:%s", err) // handle error
+	}
+	ctx.limitget = ctxlimitget
+
+	ctxlimitput, err := humanize.ParseBytes(*ctx.limitputstring)
+	if err != nil {
+		return fmt.Errorf("PutLimit value - Error:%s", err) // handle error
+	}
+	ctx.limitput = ctxlimitput
 
 	if *ctx.verbose {
-		fmt.Println("limit is", humanize.Bytes(uint64(ctx.limit)), "by second")
-		fmt.Printf("approx. %sit/s.\n\n", strings.ToLower(humanize.Bytes(uint64(ctx.limit*9))))
-	}
-	if *ctx.pwd == "" {
-		*ctx.pwd = dacqname
-	}
-	if *ctx.user == "" {
-		*ctx.user = dacqname
-	}
-	if *ctx.endpoint == "" {
-		*ctx.endpoint = os.Getenv("ViewClient_Machine_Name")
+		if *ctx.uploadmode {
+			fmt.Println("limit is", humanize.Bytes(uint64(ctx.limitput)), "by second")
+			fmt.Printf("approx. %sit/s.\n\n", strings.ToLower(humanize.Bytes(uint64(ctx.limitput*9))))
+		} else {
+			fmt.Println("limit is", humanize.Bytes(uint64(ctx.limitget)), "by second")
+			fmt.Printf("approx. %sit/s.\n\n", strings.ToLower(humanize.Bytes(uint64(ctx.limitget*9))))
+		}
 	}
 	return nil
+}
+
+func sqlUpdated(ctx *context) (bool, error) {
+	var regkey registry.Key
+	var err error
+	var keyvalue registry.Key
+	slices := strings.Split(*ctx.waitingfor, "\\")
+	// log.Println("slices:", slices)
+	// log.Println("len slices:", len(slices))
+	if len(slices) > 2 {
+		location := strings.Join(slices[1:len(slices)-1], "\\")
+		// log.Println("location:", location)
+		switch strings.ToUpper(slices[0]) {
+		case "HKCU":
+			regkey = registry.CURRENT_USER
+		case "HKLM":
+			regkey = registry.LOCAL_MACHINE
+		default:
+			return false, fmt.Errorf("Bad Registry Root (HKCU|HKLM) found [%s]", strings.ToUpper(slices[0]))
+		}
+		keyvalue, err = registry.OpenKey(regkey, location, registry.QUERY_VALUE)
+	} else {
+		return false, fmt.Errorf("Bad Registry Path - Need (HKCU|HKLM) then (Root) then (Key) [%s]", *ctx.waitingfor)
+	}
+	if err != nil {
+		return false, err
+	}
+	defer keyvalue.Close()
+	if s, _, err := keyvalue.GetStringValue(slices[len(slices)-1]); err != nil {
+		return false, err
+	} else {
+		// log.Println("lu en base de registre:", s)
+		// log.Println("comparaison:", time.Now().Local().Format("02/01/2006"))
+		return s == time.Now().Local().Format("02/01/2006"), nil
+	}
+}
+
+func waitandlaunch(ctx *context) error {
+	var remainingsecs = *ctx.howlong
+	done, err := sqlUpdated(ctx)
+	if err != nil {
+		log.Println("error in first sqlUpdated?", err)
+		return fmt.Errorf("Unable to get SqlUpdated waitingfor flag [%s]", waitingfordefval)
+	}
+	if done {
+		fmt.Println("Current date is already OK in Registry")
+		return nil
+	}
+	for {
+		time.Sleep(1 * time.Second)
+		remainingsecs--
+		done, err := sqlUpdated(ctx)
+		if err != nil {
+			log.Println("error in sqlUpdated?", err)
+			return fmt.Errorf("Unable to get SqlUpdated waitingfor flag [%s], Remains %d second(s)", waitingfordefval, remainingsecs)
+		}
+		if done {
+			log.Println("Current date is already OK in Registry")
+			return nil
+		}
+		log.Printf("Sleeping 1 second, remaining %d second(s)", remainingsecs)
+		if remainingsecs <= 0 {
+			log.Printf("No update in registry. %d second(s) elapsed.", *ctx.howlong)
+			return nil
+		}
+	}
 }
 
 // VersionNum : Litteral version
@@ -307,7 +475,7 @@ func main() {
 	}
 
 	if *contexte.verbose {
-		if *contexte.reverse {
+		if *contexte.uploadmode {
 			log.Println("processing on local device", os.Getenv("COMPUTERNAME"),
 				"file comparison versus endpoint", *contexte.endpoint, "in REVERSE local to remote")
 		} else {
@@ -346,9 +514,11 @@ func main() {
 	} else {
 		fmt.Println("no copy needed.")
 	}
-	if !*contexte.reverse {
+	if !*contexte.uploadmode {
 		fmt.Printf("Starting [%s]", *contexte.cmd)
 		exec.Command(*contexte.cmd).Start()
+		fmt.Printf("[%s] started", *contexte.cmd)
+		waitandlaunch(&contexte)
 	}
 	os.Exit(0)
 }
